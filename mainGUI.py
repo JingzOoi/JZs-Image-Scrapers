823187ff73d437585129c465ac43ef89db05321a
JZ <OoiJingZhi@gmail.com>
2019-09-29 12:45:47 +0800

    changed structure since .download() is a class function now, added exceptions; compile function

--- mainGUI.py
+++ mainGUI.py
@@ -1,89 +1,73 @@
 import PySimpleGUI as sg
 import ext.pixiv as pixiv
 import ext.nhentai as nhentai
-from ext.misc import Image
-from time import sleep
-import os
+import ext.misc as misc
+import requests
+
+sess = requests.Session()
 
 
-def batch_download_GUI(image_url_list: list, item):
-    image_category = item.category
-    metadata = str(item)
-    total = len(image_url_list)
-    total_content, total_time = 0, 0
-    for num, image_url in enumerate(image_url_list, start=1):
-        img = Image(image_url, image_category)
-        img.download()
-        total_content += img.size
-        total_time += img.time
-        t = sg.OneLineProgressMeter(
-            "Downloading image(s)...",
-            num,
-            total,
-            '_one_line_',
-            f"{round((total_content/total_time)/1000, 2):,} kb/s",
-            orientation='h',
-
-        )
-        if t is False:
-            break
-        sleep(img.time)
-    with open(os.path.join(image_category, 'metadata.txt'), 'w') as f:
-        f.write(metadata)
-    sg.PopupOK(
-        f"Done!\nDownload folder: \n{image_category}", title="Downloading complete")
-
-
-def parse_url(url, return_details: bool = False):
-
-    if 'pixiv.net' in url:
-        try:
-            illust = pixiv.Illust(url)
-        except pixiv.AlbumNotFoundException:
-            raise pixiv.AlbumNotFoundException
-        else:
-            if return_details is True:
-                return illust, 'pixiv'
-            else:
-                batch_download_GUI(illust.pages["original"], illust)
-
-    elif 'nhentai.net' in url:
-        try:
-            gallery = nhentai.Gallery(url)
-        except nhentai.AlbumNotFoundException:
-            raise nhentai.AlbumNotFoundException
-        else:
-            if return_details is True:
-                return gallery, 'nhentai'
-            else:
-                batch_download_GUI(gallery.get_page_images(), gallery)
-
+def parse_url(url: str, return_details: bool = False):
+    if 'pixiv' in url and 'artworks' in url:
+        album = pixiv.Illust(url, session=sess)
+    elif 'pixiv' in url and 'member' in url:
+        album = pixiv.User(url, session=sess)
+    elif 'nhentai' in url:
+        album = nhentai.Gallery(url, session=sess)
     else:
-        sg.PopupError("Site not supported!")
+        raise misc.AlbumNotFoundException
+
+    if return_details is True:
+        return repr(album), str(album)
+    else:
+        album.download()
+        sg.PopupOK("Completed.", title="Download operations complete.")
+
+
+class SiteNotSupportedException(Exception):
+    pass
 
 
 layout = [
     [sg.Text("URL: "), sg.InputText(key='_url_')],
-    [sg.Ok(), sg.Button("Check", key='_check_'), sg.Cancel()]
+    [sg.Button("Download", key='_download_'),
+     sg.Button("Check", key='_check_'),
+     sg.Button("Compile folder...", key='_compile_'),
+     sg.Cancel()]
 ]
 
 window = sg.Window("JZ's Image Scraper w/GUI v0.1", layout=layout)
 
 while True:
     event, values = window.Read()
-    if event in ('None', 'Cancel'):
+    if event in (None, 'Cancel'):
         break
-    elif event == '_check_':
-        url = values['_url_']
+    else:
+        url = values["_url_"]
         try:
-            details = parse_url(url, return_details=True)
-        except:
-            sg.PopupError("Album not found!")
-        else:
-            sg.PopupOK(details[0], title=f"Details for {details[1]} item")
-    elif event == 'Ok':
-        url = values['_url_']
-        try:
-            parse_url(url)
-        except:
-            sg.PopupError("Album not found!")
+            if event == '_check_':
+                item = parse_url(url, return_details=True)
+                sg.PopupOK(
+                    item[0], title=f"Showing results for item of type {item[1]}.")
+            elif event == "_download_":
+                parse_url(url)
+            elif event == "_compile_":
+                folder = sg.PopupGetFolder("Select folder to compile...")
+                if folder is None:
+                    continue
+                else:
+                    misc.compile_from_folder(folder)
+
+        except misc.AlbumNotFoundException:
+            sg.PopupError("Album not found.", title="AlbumNotFoundException")
+
+        except SiteNotSupportedException:
+            sg.PopupError("Site not supported!",
+                          title="SiteNotSupportedException")
+
+        except pixiv.UserNotAuthorisedException:
+            sg.PopupError(
+                "An error occured with the Pixiv artwork. \nIf the problem persists, contact the developer.", title="UserNotAuthorisedException")
+
+        except Exception:
+            sg.PopupError("An error has occured.", title="Exception")
